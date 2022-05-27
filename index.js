e2186a24c2c035af47f2cd2136cf77064296db7f
cbbezerra <carlafbelfort@gmail.com>
2022-05-27 17:59:11 -0300

    implementando botão "tente novamente"

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const numeroSecreto = parseInt(Math.random() * 26);
+let inputValor = document.getElementById("valor");
+inputValor.focus();
+let numeroSecreto = gerarNumeroSecreto()
+let resultado = document.getElementById("resultado");
+
+function gerarNumeroSecreto(){
+    return parseInt(Math.random() * 26);
+}
+
+function reiniciar(){
+    gerarNumeroSecreto();
+    inputValor.value = "";
+    resultado.innerHTML = "";
+    inputValor.focus();
+}
 
 function chutar(){
-    let resultado = document.getElementById("resultado");
-    let valor = parseInt(document.getElementById("valor").value);
+    let valor = parseInt(inputValor.value);
+
     if(isNaN(valor)){
-        resultado.innerHTML = "Você precisa digitar um número válido entre 0 e 25"
+        resultado.innerHTML = `Você precisa digitar um número válido entre 0 e 25`
     } else if(valor == numeroSecreto){
-        resultado.innerHTML = "Parabéns, você acertou!"
+        resultado.innerHTML = `Parabéns, você acertou!<br><button class="button" onclick="reiniciar()">Tente novamente</button>`
     } else if(valor < 0 || valor > 25){
-        resultado.innerHTML = "Você precisa digitar um número entre 0 e 25"
+        resultado.innerHTML = `Você precisa digitar um número entre 0 e 25`
     } else{
-        resultado.innerHTML = "Você errou! O número era " + numeroSecreto
+        resultado.innerHTML = (`Você errou! O número era ${numeroSecreto} <br> <button class="button" onclick="reiniciar()">Tente novamente</button>`);
     }
 
+    inputValor.value = "";
+    inputValor.focus();
+
 }
+
